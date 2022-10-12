968fc84bf4715863fe920f95647f42176844184f
JorgeM-M <108438082+JorgeM-M@users.noreply.github.com>
2022-10-11 19:50:34 -0700

    added background and player

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const canvas = document.querySelector("canvas")
+const c = canvas.getContext("2d")
 
+canvas.width = 64 * 16 //1024
+canvas.height = 64 * 9 //576
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+        this.width = 100
+        this.height = 100
+    }
+
+    draw() {
+        c.fillStyle = "aqua"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player()
+player.draw()
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "grey"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+}
+animate()
